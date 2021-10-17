e724635232fc0e0ce2699233016c6db129970925
Shayan Aamir <60174079+shayanaamir@users.noreply.github.com>
2021-10-17 02:22:57 -0700

    Extract function complete
    Extract func now returns a DataFrame with all features extracted

--- extract.py
+++ extract.py
@@ -2,57 +2,58 @@ import librosa
 import numpy as np
 import pandas as pd
 
-def extract_features(name):
+def extract_features(name, filepath="./music/"):
     data = {}
-    try:
-        x, sr = librosa.load(name, sr=None, mono=True)  # kaiser_fast
+    if filepath!=None:
+        filepath += name
 
-        data['id'] = name
+    x, sr = librosa.load(filepath, sr=None, mono=True)  # kaiser_fast
 
-        f = librosa.feature.zero_crossing_rate(x, frame_length=2048, hop_length=512)
-        data['zcr'] = f
+    data['id'] = name
 
-        cqt = np.abs(librosa.cqt(x, sr=sr, hop_length=512, bins_per_octave=12,
-                                 n_bins=7*12, tuning=None))
-        assert cqt.shape[0] == 7 * 12
-        assert np.ceil(len(x)/512) <= cqt.shape[1] <= np.ceil(len(x)/512)+1
+    f = librosa.feature.zero_crossing_rate(x, frame_length=2048, hop_length=512)
+    data['zcr'] = [f]
 
-        f = librosa.feature.chroma_cqt(C=cqt, n_chroma=12, n_octaves=7)
-        data['chroma_cqt'] = f
-        f = librosa.feature.chroma_cens(C=cqt, n_chroma=12, n_octaves=7)
-        data['chroma_cens'] =  f
-        f = librosa.feature.tonnetz(chroma=f)
-        data['tonnetz'] =  f
+    cqt = np.abs(librosa.cqt(x, sr=sr, hop_length=512, bins_per_octave=12,
+                                n_bins=7*12, tuning=None))
+    assert cqt.shape[0] == 7 * 12
+    assert np.ceil(len(x)/512) <= cqt.shape[1] <= np.ceil(len(x)/512)+1
 
-        del cqt
-        stft = np.abs(librosa.stft(x, n_fft=2048, hop_length=512))
-        assert stft.shape[0] == 1 + 2048 // 2
-        assert np.ceil(len(x)/512) <= stft.shape[1] <= np.ceil(len(x)/512)+1
-        del x
+    f = librosa.feature.chroma_cqt(C=cqt, n_chroma=12, n_octaves=7)
+    data['chroma_cqt'] = [f]
+    f = librosa.feature.chroma_cens(C=cqt, n_chroma=12, n_octaves=7)
+    data['chroma_cens'] = [f]
+    f = librosa.feature.tonnetz(chroma=f)
+    data['tonnetz'] = [f]
 
-        f = librosa.feature.chroma_stft(S=stft**2, n_chroma=12)
-        data['chroma_stft'] = f
+    del cqt
+    stft = np.abs(librosa.stft(x, n_fft=2048, hop_length=512))
+    assert stft.shape[0] == 1 + 2048 // 2
+    assert np.ceil(len(x)/512) <= stft.shape[1] <= np.ceil(len(x)/512)+1
+    del x
 
-        f = librosa.feature.rmse(S=stft)
-        data['rmse'] =f
+    f = librosa.feature.chroma_stft(S=stft**2, n_chroma=12)
+    data['chroma_stft'] = [f]
 
-        f = librosa.feature.spectral_centroid(S=stft)
-        data['spectral_centroid'] =  f
-        f = librosa.feature.spectral_bandwidth(S=stft)
-        data['spectral_bandwidth'] = f
-        f = librosa.feature.spectral_contrast(S=stft, n_bands=6)
-        data['spectral_contrast'] = f
-        f = librosa.feature.spectral_rolloff(S=stft)
-        data['spectral_rolloff'] = f
+    f = librosa.feature.rms(S=stft)
+    data['rmse'] = [f]
 
-        mel = librosa.feature.melspectrogram(sr=sr, S=stft**2)
-        del stft
-        f = librosa.feature.mfcc(S=librosa.power_to_db(mel), n_mfcc=20)
-        data['mfcc'] = f
+    f = librosa.feature.spectral_centroid(S=stft)
+    data['spectral_centroid'] = [f]
+    f = librosa.feature.spectral_bandwidth(S=stft)
+    data['spectral_bandwidth'] = [f]
+    f = librosa.feature.spectral_contrast(S=stft, n_bands=6)
+    data['spectral_contrast'] = [f]
+    f = librosa.feature.spectral_rolloff(S=stft)
+    data['spectral_rolloff'] = [f]
 
-        features = pd.DataFrame(data=data, columns=data.keys)
-    except:
-        exit(1)
+    mel = librosa.feature.melspectrogram(sr=sr, S=stft**2)
+    del stft
+    f = librosa.feature.mfcc(S=librosa.power_to_db(mel), n_mfcc=20)
+    data['mfcc'] = [f]
+
+    features = pd.DataFrame(data)
     return features
 
-d = extract_features()
+d = extract_features("The Kid LAROI, Justin Bieber - STAY (Official Video).wav")
+print(d)
